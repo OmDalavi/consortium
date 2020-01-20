f749bac127dd2e63ba35dde6d827c80fb59d51d2
JituR101 <jitendra98rahangdale@gmail.com>
2020-01-21 02:27:34 +0530

    swades questionniare released

--- success.php
+++ success.php
@@ -1,57 +1,47 @@
 <!DOCTYPE html>
-  <html>
-    <head>
-
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" href="user-css/login-style.css">
-        <link rel="shortcut icon" type="image/png" href="images/conso-icon.png">
-        <title>SWADES'20 | Questionnaire</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="css/swades.css">
-        <?php include("includes/head.php")?>
-
-
-    </head>
-    <body color="white">
-
-        <?php include("includes/header.php")?>
-
-      
-          
-          </br>
-           </br>
-            </br>
-             </br> 
-              </br>
-              </br>
-          <button class="btn btn-outline-success my-2 my-sm-0" style="margin-left: 20%;">Swadesh Round 1: Questionnaire</button>
-        </br>
-        </br>
-        <div style="margin-bottom: 10px;" class="start-test instructions" >
-
-          <h5 style="color: white;">Well Done Team! </h5>
-         
-           <div style="color: white;">You Have Completed Swadesh Round 1.</div> 
-           <div style="color: white;"> Your Answer were Submitted Successfully.</div>
-          <div style="color: white;">Result Will Be Mailed to You. </div>
-           <div style="color: white;">Regards Team Swadesh,E-Cell VNIT. </div>
-          
-        
-        </div>
+<html>
+  <?php $pagetitle = 'Pay TREC | Consortium'; ?>
+  <?php include('includes/head.php'); ?>
+  <body class="back">
+    <?php include('includes/header.php'); ?>
+    <div id="register">
+        <div class="g-container--sm g-padding-y-80--xs g-padding-y-125--sm">
+                <div class="g-text-center--xs g-color--white-opacity g-margin-b-60--xs">
+                      <h2 class="g-color--white-opacity">Round 1 Completed Successfully!
+                    </h2>
 
 
-        </div>
+                    <div class="col-md-8 col-md-offset-2" style="text-align: center;">
+                      <p class="g-color--white-opacity g-font-size-16--sm">
+
+                          <b>Your answers has been submitted.<b><br>
 
 
 
+                      </p>
 
-</script>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+                        <p class="g-color--white-opacity g-font-size-16--sm">
 
-        <?php include("includes/footer_landing.php");?>
-        <!--========== END FOOTER ==========-->
-        <?php include("includes/script.php");?>
-</body>
+
+                             You will receive results via email<br>
+                             <br>
+
+                             <div class="wow fadeInLeft" data-wow-duration=".3" data-wow-delay=".5s">
+                                 <a id="reg_button" href="dashboard.php" title="dashboard">
+                                     <!--<i class="s-icon s-icon--lg s-icon--white-bg g-radius--circle ti-arrow-down"></i>-->
+                                     <span class="text-uppercase s-btn s-btn--xs s-btn--white-brd g-radius--50">Back to Dashboard</span>
+                                 </a>
+                             </div>
+
+
+                        </p>
+
+
+                    </div>
+                </div>
+            </div>
+    </div>
+    <?php include("includes/footer_landing.php");?>
+    <?php include("includes/script.php");?>
+  </body>
 </html>
