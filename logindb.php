2340cab17dce1a6baf56d1ffce66beacc5459949
JituR101 <jitendra98rahangdale@gmail.com>
2020-01-22 16:52:41 +0530

    Added CEO

--- logindb.php
+++ logindb.php
@@ -97,7 +97,7 @@ else if(isset($_POST['login'])){
                                         <option value="Swadesh">Swades</option>
                                         <option value="trec">TREC</option>
                                         <option value="nirmaan_team">Nirmaan Team</option>
-                                      
+                                        <option value="CEO">CEO</option>
                                         <!-- <option value="Pitch_Perfect">Pitch Perfect</option>
                                         <option value="renderico_team">Render Ico</option>
                                         <option value="BizQuiz_team">Bizquiz</option>
