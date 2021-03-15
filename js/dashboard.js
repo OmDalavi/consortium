0615f831f0a8001e95ea395a551945affeea84d0
amit-420 <amitsawant2424@gmail.com>
2021-03-15 22:44:07 +0530

    some minor changes

--- js/dashboard.js
+++ js/dashboard.js
@@ -6,13 +6,24 @@ $(".product-card__item-grid").css({
 function closemodel(event){
     $("#"+event).css({"display": "none"},100);
 }
-// payment script
+
+
+
+
+
+// ceo
 $("#ceoclick").click(function(){
  $("#paylinkceo").css({"display":"block"});
  $("#paylinkceo").animate({opacity: 1}, 1000);
  var y = $("#paylinkceo").offset().top;
    $("html ,body").animate({ scrollTop: y},200);
 });
+
+
+
+
+
+// wall street
 
 $("#wallstreetclick").click(function(){
 $("#paylinkwallstreet").css({"display":"block"});
@@ -21,52 +32,15 @@ var y = $("#paylinkwallstreet").offset().top;
   $("html ,body").animate({ scrollTop: y},200);
 });
 
+
+
+
+// adventure
+
 $("#adventureclick").click(function(){
   $("#paylinkadventure").css({"display":"block"});
   $("#paylinkadventure").animate({opacity: 1}, 1000);
   var y = $("#paylinkadventure").offset().top;
-    $("html ,body").animate({ scrollTop: y},200);
-});
-
-$("#swadesclick").click(function(){
-  $("#paylinkswades").css({"display":"block"});
-  $("#paylinkswades").animate({opacity: 1}, 1000);
-  var y = $("#paylinkswades").offset().top;
-    $("html ,body").animate({ scrollTop: y},200);
-});
-
-$("#rendericoclick").click(function(){
-  $("#renderico").css({"display":"block"});
-  $("#renderico").animate({opacity: 1}, 1000);
-  var y = $("#renderico").offset().top;
-    $("html ,body").animate({ scrollTop: y},200);
-});
-
-$("#bizquizclick").click(function(){
-  $("#bizquiz").css({"display":"block"});
-  $("#bizquiz").animate({opacity: 1}, 1000);
-  var y = $("#bizquiz").offset().top;
-    $("html ,body").animate({ scrollTop: y},200);
-});
-
-$("#war_of_worldsclick").click(function(){
-  $("#paylinkwar_of_worlds").css({"display":"block"});
-  $("#paylinkwar_of_worlds").animate({opacity: 1}, 1000);
-  var y = $("#paylinkwar_of_worlds").offset().top;
-    $("html ,body").animate({ scrollTop: y},200);
-});
-
-$("#operation_researchclick").click(function(){
-  $("#operation_research").css({"display":"block"});
-  $("#operation_research").animate({opacity: 1}, 1000);
-  var y = $("#operation_research").offset().top;
-    $("html ,body").animate({ scrollTop: y},200);
-});
-
-$("#pitch_mantraclick").click(function(){
-  $("#pitch_mantra").css({"display":"block"});
-  $("#pitch_mantra").animate({opacity: 1}, 1000);
-  var y = $("#pitch_mantra").offset().top;
     $("html ,body").animate({ scrollTop: y},200);
 });
 
@@ -84,6 +58,18 @@ $("#details_clickadventure").click(function(){
     $("html ,body").animate({ scrollTop: y},200);
 });
 
+
+
+
+// swades
+
+$("#swadesclick").click(function(){
+  $("#paylinkswades").css({"display":"block"});
+  $("#paylinkswades").animate({opacity: 1}, 1000);
+  var y = $("#paylinkswades").offset().top;
+    $("html ,body").animate({ scrollTop: y},200);
+});
+
 $("#details_clickswades").click(function(){
   $("#detailsswades").css({"display":"block"});
   $("#detailsswades").animate({opacity: 1}, 1000);
@@ -96,3 +82,87 @@ $("#adteam_clickswades").click(function(){
   var y = $("#membersswades").offset().top;
     $("html ,body").animate({ scrollTop: y},200);
 });
+
+
+
+
+
+// render ico
+
+$("#rendericoclick").click(function(){
+  $("#renderico").css({"display":"block"});
+  $("#renderico").animate({opacity: 1}, 1000);
+  var y = $("#renderico").offset().top;
+    $("html ,body").animate({ scrollTop: y},200);
+});
+
+
+
+
+// bizquiz
+
+$("#bizquizclick").click(function(){
+  $("#bizquiz").css({"display":"block"});
+  $("#bizquiz").animate({opacity: 1}, 1000);
+  var y = $("#bizquiz").offset().top;
+    $("html ,body").animate({ scrollTop: y},200);
+});
+
+
+
+
+
+// war of worlds 
+
+$("#war_of_worldsclick").click(function(){
+  $("#paylinkwar_of_worlds").css({"display":"block"});
+  $("#paylinkwar_of_worlds").animate({opacity: 1}, 1000);
+  var y = $("#paylinkwar_of_worlds").offset().top;
+    $("html ,body").animate({ scrollTop: y},200);
+});
+
+
+
+// Operation research
+
+$("#operation_researchclick").click(function(){
+  $("#operation_research").css({"display":"block"});
+  $("#operation_research").animate({opacity: 1}, 1000);
+  var y = $("#operation_research").offset().top;
+    $("html ,body").animate({ scrollTop: y},200);
+});
+
+$("#operation_researchclick").click(function(){
+  $("#paylinkoperation_research").css({"display":"block"});
+  $("#paylinkoperation_research").animate({opacity: 1}, 1000);
+  var y = $("#operation_research").offset().top;
+    $("html ,body").animate({ scrollTop: y},200);
+});
+
+$("#details_clickoperation_research").click(function(){
+  $("#detailsoperation_research").css({"display":"block"});
+  $("#detailsoperation_research").animate({opacity: 1}, 1000);
+  var y = $("#operation_research").offset().top;
+    $("html ,body").animate({ scrollTop: y},200);
+});
+
+$("#adteam_clickoperation_research").click(function(){
+  $("#membersoperation_research").css({"display":"block"});
+  $("#membersoperation_research").animate({opacity: 1}, 1000);
+  var y = $("#operation_research").offset().top;
+    $("html ,body").animate({ scrollTop: y},200);
+});
+
+
+
+// pitch mantra
+$("#pitch_mantraclick").click(function(){
+  $("#pitch_mantra").css({"display":"block"});
+  $("#pitch_mantra").animate({opacity: 1}, 1000);
+  var y = $("#pitch_mantra").offset().top;
+    $("html ,body").animate({ scrollTop: y},200);
+});
+
+
+
+
